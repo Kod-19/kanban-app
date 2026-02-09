9005bb7f1d0e7f18822cbf40a752ad985467966f
Kwame Dawson <dawsonkwame9@gmail.com>
2026-02-09 23:51:33 +0000

    added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='bg-black'>
+      <Navbar />
+    </div>
   )
 }
 
