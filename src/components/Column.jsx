c2855c62fd6ec63686178a08a9e7ad584f8fa46b
Kwame Dawson <dawsonkwame9@gmail.com>
2026-02-12 13:59:19 +0000

    commit

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import circle from '../assets/unchecked_btn.png'
 
-const Column = ({ title, todo }) => {
+const Column = ({title}) => {
   return (
     <div className='bg-gray-200 rounded-md h-150'>
       <h3 className='font-semibold text-lg p-4'>{title}</h3>
